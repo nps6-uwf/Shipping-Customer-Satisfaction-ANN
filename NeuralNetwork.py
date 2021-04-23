567bb42df1d5030e0de8288f23b27c295567751f
nps6-uwf <78291978+nps6-uwf@users.noreply.github.com>
2021-04-23 09:34:09 +0700

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,7 +2,6 @@
 # Date: 4/22/2021
 # Version: V1
 # Overview:  Implementing a ANN from scratch to classify.
-# Source: https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
 import numpy as np
 from random import shuffle
 
@@ -95,23 +94,5 @@ class NeuralNetwork:
         outputs = self.forward_propagate(network, test_vector)
         return outputs.index(max(outputs))
 
-if False:
-    # Test training backprop algorithm
-    dataset = [[2.7810836,2.550537003,0],
-        [1.465489372,2.362125076,0],
-        [3.396561688,4.400293529,0],
-        [1.38807019,1.850220317,0],
-        [3.06407232,3.005305973,0],
-        [7.627531214,2.759262235,1],
-        [5.332441248,2.088626775,1],
-        [6.922596716,1.77106367,1],
-        [8.675418651,-0.242068655,1],
-        [7.673756466,3.508563011,1]]
-    n_inputs = len(dataset[0]) - 1
-    n_outputs = len(set([row[-1] for row in dataset]))
-    NN = NeuralNetwork(n_inputs, 5, n_outputs)
-    NN.train_network(NN.network, dataset, 0.5, 20, n_outputs)
-    for layer in NN.network:
-        print(layer)
 
 
